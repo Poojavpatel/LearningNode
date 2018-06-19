752436519b0ce9f09427a237936eda58b6b146af
Pooja patel <poojavpatel71@gmail.com>
2018-06-19 22:43:12 +0530

    response based on url

--- connection.js
+++ connection.js
@@ -2,7 +2,19 @@
 //Http module
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req , res) => {
+
+    if(req.url === "/"){
+        res.write("hello friends ! chai pi lo");
+        res.end();
+    }
+
+    if(req.url === "/api/courses"){
+        res.write(JSON.stringify( ['1','2','3'] ));
+        res.end();
+    }
+
+});
 
 server.on('connection',(socket) => {
     console.log('New connection established');
